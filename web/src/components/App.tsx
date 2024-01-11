fd3d6ed625b20e836b2a7a9dbca32c8389ef57b9
vipex <101529155+vipexv@users.noreply.github.com>
2024-01-11 23:18:38 +0100

    style: continued progress on nui

--- web/src/components/App.tsx
+++ web/src/components/App.tsx
@@ -1,11 +1,43 @@
+import { Divider } from "@mantine/core";
+import { Cog, ShieldAlert } from "lucide-react";
 import React, { useEffect, useState } from "react";
 import { useNuiEvent } from "../hooks/useNuiEvent";
 import { debugData } from "../utils/debugData";
 import { fetchNui } from "../utils/fetchNui";
 import { isEnvBrowser } from "../utils/misc";
-import { Separator } from "@/components/ui/separator";
 import "./App.css";
-import { MessageSquareWarning, ShieldAlert } from "lucide-react";
+import { Button } from "./ui/button";
+import {
+  Cloud,
+  CreditCard,
+  Github,
+  Keyboard,
+  LifeBuoy,
+  LogOut,
+  Mail,
+  MessageSquare,
+  Plus,
+  PlusCircle,
+  Settings,
+  User,
+  UserPlus,
+  Users,
+} from "lucide-react";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuPortal,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 debugData([
   {
@@ -13,6 +45,15 @@ debugData([
     data: true,
   },
 ]);
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const reports = Array.from({ length: 100 }, (_, index) => ({
+  id: index,
+  description: "Very Very racist!",
+  state: {
+    concluded: false,
+  },
+}));
 
 const App: React.FC = () => {
   const [visible, setVisible] = useState(false);
@@ -37,14 +78,106 @@ const App: React.FC = () => {
   return (
     <>
       <div className="flex w-[100dvw] h-[100dvh] justify-center items-center">
-        <div className="min-w-[45dvw] min-h-[35dvw] bg-primary">
+        <div className="min-w-[45dvw] min-h-[35dvw] bg-primary rounded-[2px]">
           <div className="flex items-center">
-            <h1 className="m-2 relative flex justify-center items-center p-1 bg-secondary` bg-opacity-50 font-main text-white text-xl border">
-              <ShieldAlert size={18} className="mr-1 text-red-500" />
+            <h1 className="m-2 relative flex justify-center items-center rounded p-1 font-main text-white text-xl border-[2px]">
+              <ShieldAlert size={18} className="mr-1 text-blue-400" />
               Report Menu
             </h1>
+            <Button className="ml-auto relative rounded flex justify-center items-center mr-1 border-[2px] bg-opacity-50 font-main text-white text-xl hover:bg-secondary">
+              <Cog size={14} />
+            </Button>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button className="border-[2px] rounded hover:bg-secondary">
+                  {" "}
+                  <User className="h-4 w-4" />
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent className="w-56">
+                <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuGroup>
+                  <DropdownMenuItem>
+                    <User className="mr-2 h-4 w-4" />
+                    <span>Profile</span>
+                    <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
+                  </DropdownMenuItem>
+                  <DropdownMenuItem>
+                    <CreditCard className="mr-2 h-4 w-4" />
+                    <span>Billing</span>
+                    <DropdownMenuShortcut>⌘B</DropdownMenuShortcut>
+                  </DropdownMenuItem>
+                  <DropdownMenuItem>
+                    <Settings className="mr-2 h-4 w-4" />
+                    <span>Settings</span>
+                    <DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
+                  </DropdownMenuItem>
+                  <DropdownMenuItem>
+                    <Keyboard className="mr-2 h-4 w-4" />
+                    <span>Keyboard shortcuts</span>
+                    <DropdownMenuShortcut>⌘K</DropdownMenuShortcut>
+                  </DropdownMenuItem>
+                </DropdownMenuGroup>
+                <DropdownMenuSeparator />
+                <DropdownMenuGroup>
+                  <DropdownMenuItem>
+                    <Users className="mr-2 h-4 w-4" />
+                    <span>Team</span>
+                  </DropdownMenuItem>
+                  <DropdownMenuSub>
+                    <DropdownMenuSubTrigger>
+                      <UserPlus className="mr-2 h-4 w-4" />
+                      <span>Invite users</span>
+                    </DropdownMenuSubTrigger>
+                    <DropdownMenuPortal>
+                      <DropdownMenuSubContent>
+                        <DropdownMenuItem>
+                          <Mail className="mr-2 h-4 w-4" />
+                          <span>Email</span>
+                        </DropdownMenuItem>
+                        <DropdownMenuItem>
+                          <MessageSquare className="mr-2 h-4 w-4" />
+                          <span>Message</span>
+                        </DropdownMenuItem>
+                        <DropdownMenuSeparator />
+                        <DropdownMenuItem>
+                          <PlusCircle className="mr-2 h-4 w-4" />
+                          <span>More...</span>
+                        </DropdownMenuItem>
+                      </DropdownMenuSubContent>
+                    </DropdownMenuPortal>
+                  </DropdownMenuSub>
+                  <DropdownMenuItem>
+                    <Plus className="mr-2 h-4 w-4" />
+                    <span>New Team</span>
+                    <DropdownMenuShortcut>⌘+T</DropdownMenuShortcut>
+                  </DropdownMenuItem>
+                </DropdownMenuGroup>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>
+                  <Github className="mr-2 h-4 w-4" />
+                  <span>GitHub</span>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <LifeBuoy className="mr-2 h-4 w-4" />
+                  <span>Support</span>
+                </DropdownMenuItem>
+                <DropdownMenuItem disabled>
+                  <Cloud className="mr-2 h-4 w-4" />
+                  <span>API</span>
+                </DropdownMenuItem>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>
+                  <LogOut className="mr-2 h-4 w-4" />
+                  <span>Log out</span>
+                  <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
           </div>
-          <Separator />
+          {/* <Separator /> */}
+          <Divider size="xs" />
         </div>
       </div>
     </>
