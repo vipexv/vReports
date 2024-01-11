1600e7050770b015209fa8a92720d0f90cbc08d2
vipex <101529155+vipexv@users.noreply.github.com>
2024-01-11 22:31:04 +0100

    chore: add mantine for future reference

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,10 +1,15 @@
+import { MantineProvider } from "@mantine/core";
+import "@mantine/core/styles.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App";
+
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <MantineProvider defaultColorScheme="dark">
+      <App />
+    </MantineProvider>
   </React.StrictMode>
 );
