6008694ec7873694cdd8a129261041e93ca24bbd
vipex <101529155+vipexv@users.noreply.github.com>
2024-01-18 19:57:05 +0100

    refactor: update currentVersion var to 1.0.4

--- server/sv_utils.lua
+++ server/sv_utils.lua
@@ -44,7 +44,7 @@ end
 VersionCheck = function(repository)
     local resource = GetInvokingResource() or GetCurrentResourceName()
 
-    local currentVersion = 'v1.0.3'
+    local currentVersion = 'v1.0.4'
 
     if currentVersion then
         currentVersion = currentVersion:match('%d+%.%d+%.%d+')
