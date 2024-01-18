bafe6382260c329e1b2e02f5c7177915fa81d757
vipex <101529155+vipexv@users.noreply.github.com>
2024-01-18 18:13:39 +0100

    revert: posted my test bot token since gitignore does not wanna work

--- sv_config.lua
+++ sv_config.lua
@@ -1,4 +1,4 @@
 SVConfig = {
-    ["Bot Token"] = "OTc1MTgzMjI0NjcyMjM5NjU2.GM0FyC.Iv3LDPbv-hZkTUFMe6gHXd-Geohc1fB7Wg1504",
-    ["Guild ID"] = "839129247842828338",
+    ["Bot Token"] = "",
+    ["Guild ID"] = "",
 }
